b1004e3b6ea0d87f75421521b4efec52d38ce6a0
FancyPants278 <leonsol.eduardo@gmail.com>
2021-07-21 18:55:53 -0500

    Updated Diving Categories visualization in Atlas_de_buceo_app.Rmd. Added new version of MPAs Metadata (MPA_metadata_v.1.01_21072021.csv). Added new section in 00-all_data_wrangling.R for updating MPA_Mx shapefile.

--- 00-all_data_wrangling.R
+++ 00-all_data_wrangling.R
@@ -4,6 +4,7 @@ library(rnaturalearth)
 library(ggthemes)
 library(patchwork)
 
+#Old shapefile
 spdf_mx <- st_transform(st_as_sf(ne_countries(country = 'mexico')), crs = 4326)
 
 
@@ -37,4 +38,39 @@ test <- test %>%
                core_zone = ifelse(Pesca == "Prohibida", "Pesca Prohibida", "Amortiguamiento"),
                protection_level = ifelse(is.na(core_zone), "Sin Proteccion", core_zone)) %>% 
         select(-core_zone) %>% 
-        mutate(protection_level = factor(protection_level, levels = c("Sin Proteccion", "Amortiguamiento", "Pesca Prohibida"))) 
+        mutate(protection_level = factor(protection_level, levels = c("Sin Proteccion", "Amortiguamiento", "Pesca Prohibida")))
+
+#Updated MPA & Metadata
+
+mpa_v1 <- st_read("shp/MPA_MX_v1.0_01032021/MPA_MX_c1-0.shp")
+
+metadata_v1.01 <- read.csv("data/MPA_metadata_v.1.01_21072021.csv", na.strings ="")
+metadata_v1.01[, 23] <- NULL
+
+metadata_v1.01 <- metadata_v1.01 %>% 
+        rename(
+                pesca_comercial= Pesca.Comercial,
+                pesca_deportiva= Pesca.Deportiva,
+                Pesca= Pesca,
+                Buceo= Cat_Buceo,
+                PdM= Lo.que.dice.el.PM
+        )
+mpa_v1 <- mpa_v1 %>%
+        select(ANP, ID, nombr_1)
+
+mpa_v1.01 <- merge(mpa_v1, metadata_v1.01, by = c("ID", "ANP"))
+
+mpa_v1.01$Pesca <- replace_na(mpa_v1.01$Pesca, "NA")
+
+mpa_v1.01$Pesca <- factor(mpa_v1.01$Pesca, levels = c("NA", "ND", "NO", "SI"), labels = c("Sin manejo", "No especificado", "Prohibida", "Permitida"))
+
+mpa_v1.01$Buceo <- replace_na(mpa_v1.01$Buceo, "NA")
+
+mpa_v1.01$Buceo <- factor(mpa_v1.01$Buceo, levels = c("NA", "NO SE MENCIONA", "AMBIGUO", "PROHIBIDO", "PERMITIDO"), labels = c("Sin manejo", "No se menciona", "Ambiguo", "Prohibido", "Permitido"))
+
+st_write(mpa_v1.01, "shp/MPA_MX_v1.01_21072021/MPA_MX_c1-01.shp")
+
+unique(final_mpa$Buceo)
+unique(mpa_v1.01$Pesca)
+
+        
